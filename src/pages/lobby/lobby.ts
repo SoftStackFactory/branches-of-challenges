00b0c45f665504fdf0de18ed3682eb6585a9e00c
jbrownssf <jpbrown@softstackfactory.org>
2017-03-28 00:04:56 +0000

    Challenge: Unknown transpile error

--- src/pages/lobby/lobby.ts
+++ src/pages/lobby/lobby.ts
@@ -12,12 +12,26 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'lobby.html'
 })
 export class LobbyPage {
+  currentSegment = "drafts"
+  posts: String[] = [
+    "2017-03-22T23:55:42.477Z",
+    "2017-03-23T23:55:42.477Z",
+    "2017-03-24T23:55:42.477Z",
+    "2017-03-25T23:55:42.477Z",
+    "2017-03-26T23:55:42.477Z",
+    "2017-03-27T23:55:42.477Z"
+  ]
+  drafts: String[] = {
+    "2017-03-28T23:55:42.477Z",
+    "2017-03-29T23:55:42.477Z",
+    "2017-03-30T23:55:42.477Z",
+    "2017-03-31T23:55:42.477Z"
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LobbyPage');
   }
-  currentSegment = "drafts"
 
 }
